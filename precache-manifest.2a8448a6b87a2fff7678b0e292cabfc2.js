062cb084fe5cfef55e1fda9606822dd98a898c40
ermyas7 <ermyas.fekadu@gmail.com>
2019-03-13 03:52:46 +0530

    Updates

---
+++ precache-manifest.2a8448a6b87a2fff7678b0e292cabfc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b4839081b16fefb6ab",
+    "url": "/ermyas7/directory/static/css/main.bed9f1bb.chunk.css"
+  },
+  {
+    "revision": "54b4839081b16fefb6ab",
+    "url": "/ermyas7/directory/static/js/main.cfe908b8.chunk.js"
+  },
+  {
+    "revision": "1b830cc6107d68aae756",
+    "url": "/ermyas7/directory/static/js/runtime~main.4b1ce1af.js"
+  },
+  {
+    "revision": "cdddc0ac95390903cfd6",
+    "url": "/ermyas7/directory/static/js/2.1874d343.chunk.js"
+  },
+  {
+    "revision": "16358d325f1470f7ff36f832816e7489",
+    "url": "/ermyas7/directory/index.html"
+  }
+];
